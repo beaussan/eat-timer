36bd3084c57d90cfccdce38669d3ff9f854bb5a3
Nicolas Beaussart <nic.beaussart@gmail.com>
2020-10-15 22:30:04 +0200

    Fix

--- src/App.js
+++ src/App.js
@@ -7,8 +7,6 @@ import eatSound from './eat.mp3';
 import Confetti from 'react-dom-confetti';
 import { useLocalStorage } from './useLocalStorage';
 
-const SECONDS_TO_EAT = 5;
-
 const config = {
   angle: 90,
   spread: 360,
@@ -22,7 +20,6 @@ const config = {
   perspective: '500px',
   colors: ['#a864fd', '#29cdff', '#78ff44', '#ff718d', '#fdff6a']
 };
-
 
 function MyTimer({ expiryTimestamp, interval }) {
   const addTime = addSeconds(interval + 1);
@@ -46,10 +43,13 @@ function MyTimer({ expiryTimestamp, interval }) {
     setTimeout(() => {
       setAlreadyStartedOnce(true);
     }, 300);
-  }
+  };
 
   return (
-    <div className="flex flex-col  items-center justify-center h-screen w-screen cursor-pointer" onClick={startTimer}>
+    <div
+      className="flex flex-col  items-center justify-center h-screen w-screen cursor-pointer"
+      onClick={startTimer}
+    >
       <Confetti active={!isRunning && alreadyStartedOnce} config={config} />
       {isRunning && (
         <div style={{ fontSize: '100px' }}>
@@ -59,9 +59,7 @@ function MyTimer({ expiryTimestamp, interval }) {
 
       <div className="h-16">
         {!isRunning ? (
-          <button
-            className="py-2 px-4 bg-green-300 rounded-lg text-green-800"
-          >
+          <button className="py-2 px-4 bg-green-300 rounded-lg text-green-800">
             Dis moi quand je peux manger !
           </button>
         ) : (
@@ -72,7 +70,6 @@ function MyTimer({ expiryTimestamp, interval }) {
   );
 }
 
-
 export default function App() {
   const [state, setState] = useState(undefined);
   const [itm, setItm] = useState(0);
@@ -81,26 +78,54 @@ export default function App() {
     if (val <= 0) {
       return;
     }
-    const newOld = [...new Set([...oldItems, val].sort((function(a, b){return a-b})))]
+    const newOld = [
+      ...new Set(
+        [...oldItems, val].sort(function (a, b) {
+          return a - b;
+        })
+      )
+    ];
     setOldItems(newOld);
     setState(val);
-  }
+  };
   const initTime = new Date();
   return (
     <div className="w-screen h-screen overflow-hidden bg-black text-white flex items-center justify-center">
-      { state ? <MyTimer expiryTimestamp={initTime} interval={state} /> : (<div className="flex flex-col">
-        <div className="flex ">
-          <input value={itm} onChange={event => setItm(parseInt(event.target.value))} type="number" className="px-3 text-black rounded-lg w-20 mr-5"/>
-          <button className="text-black bg-blue-300 text-blue-800 rounded-lg py-2 px-4" onClick={() => { setUserValue(itm)}}>Commencer !</button>
+      {state ? (
+        <MyTimer expiryTimestamp={initTime} interval={state} />
+      ) : (
+        <div className="flex flex-col">
+          <div className="flex ">
+            <input
+              value={itm}
+              onChange={(event) => setItm(parseInt(event.target.value))}
+              type="number"
+              className="px-3 text-black rounded-lg w-20 mr-5"
+            />
+            <button
+              className="text-black bg-blue-300 text-blue-800 rounded-lg py-2 px-4"
+              onClick={() => {
+                setUserValue(itm);
+              }}
+            >
+              Commencer !
+            </button>
+          </div>
+          <div className="flex mt-8 space-x-5">
+            {oldItems.map((it) => {
+              return (
+                <button
+                  key={it}
+                  className="bg-orange-300 text-orange-800 rounded-lg py-2 px-4"
+                  onClick={() => setState(it)}
+                >
+                  {it}
+                </button>
+              );
+            })}
+          </div>
         </div>
-        <div className="flex mt-8 space-x-5">
-          {oldItems.map(it => {
-            return <button key={it} className="bg-orange-300 text-orange-800 rounded-lg py-2 px-4"
-                            onClick={() => setState(it)}>{it}</button>;
-          })}
-        </div>
-      </div>)
-      }
+      )}
     </div>
   );
 }
